578ed346ec69731a1aff30e85373a0a0d161dba9
Gus Bonfant <gbonfant@gmail.com>
2013-06-24 17:05:19 +0200

    Added pure.css and fixed view inheritance

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.configure(function() {
   app.set('port', process.env.PORT || 3500);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+
+  app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function(req, res) {
-  res.render('index', {
-    title: 'Home'
-  });
+  res.render('index', { title: 'Home' });
 });
 
 app.listen(app.get('port'));
